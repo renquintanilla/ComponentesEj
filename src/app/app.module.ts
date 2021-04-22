b23ae66e3983b6b188246aa1f99ef7c58c647771
renquintanilla <50958699+renquintanilla@users.noreply.github.com>
2021-04-21 20:43:06 -0600

    Sumar y Cronómetro con Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SumarComponent } from './components/sumar/sumar.component';
+import { CronometroComponent } from './components/cronometro/cronometro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SumarComponent,
+    CronometroComponent
   ],
   imports: [
     BrowserModule
